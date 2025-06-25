dde151d3f6400512fce1a3c3792c77a7acac175e
Ali Shakeel <alishakeel567567@gmail.com>
2025-06-26 02:27:39 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 
 import HomePage from './Pages/page'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
